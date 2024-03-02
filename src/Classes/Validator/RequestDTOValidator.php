35c248a432d02cab404ac8effd46750ca808b657
SidAlex <palach.orl@mail.ru>
2024-03-02 22:41:51 +0300

    add validator

--- src/Classes/Validator/RequestDTOValidator.php
+++ src/Classes/Validator/RequestDTOValidator.php
@@ -2,7 +2,38 @@
 
 namespace Sidalex\CandidateVacancyEstimationGpt\Classes\Validator;
 
+use Sidalex\CandidateVacancyEstimationGpt\Classes\Validator\Exceptions\RequestValidateException;
+
 class RequestDTOValidator
 {
+    private \stdClass $request;
+
+    /**
+     * @param \stdClass $request
+     * @throws RequestValidateException
+     */
+    public function __construct(mixed $request)
+    {
+        if($request === false || is_null($request)){
+            throw new RequestValidateException("Request is not valid json");
+        }
+        if(!($request instanceof \stdClass)){
+            throw new RequestValidateException("Request is bad servise cann't be json decoded");
+        }
+        $this->request = $request;
+    }
+
+    /**
+     * @throws RequestValidateException
+     */
+    public function validate()
+    {
+        if (!isset($this->request->vacancyText) || empty($this->request->vacancyText)) {
+            throw new RequestValidateException("not found vacancyText property or is empty");
+        }
+        if (!isset($this->request->candidateResumeText) || empty($this->request->candidateResumeText)) {
+            throw new RequestValidateException("not found candidateResumeText property or is empty");
+        }
+    }
 
 }
